efd0866abf63e0848f94a30def0d01c992918a0e
wisskirchenj <juergen.wisskirchen@cofinpro.de>
2023-04-29 21:48:33 +0200

    Fix test in CI pipe

--- src/test/java/de/cofinpro/budget/io/StateSerializerTest.java
+++ src/test/java/de/cofinpro/budget/io/StateSerializerTest.java
@@ -3,18 +3,29 @@ package de.cofinpro.budget.io;
 import de.cofinpro.budget.model.BudgetState;
 import de.cofinpro.budget.model.Category;
 import de.cofinpro.budget.model.Purchase;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StateSerializerTest {
 
+    private static final Path DIR = Path.of("src/main/resources/data");
     private StateSerializer serializer;
-
     private BudgetState state;
+
+    @BeforeAll
+    static void createDirIfNotExists() throws IOException {
+        if (!Files.exists(DIR)) {
+            Files.createDirectory(DIR);
+        }
+    }
 
     @BeforeEach
     void setup() {
